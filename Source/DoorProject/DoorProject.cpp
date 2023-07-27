d7a5bbd68de369b47506648057a0a93708467509
Stepan <grigorian.stepan04@gmail.com>
2023-07-27 16:47:28 +0400

    Init commit

---
+++ Source/DoorProject/DoorProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DoorProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DoorProject, "DoorProject" );
+ 
