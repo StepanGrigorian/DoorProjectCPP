840b92d4e3ae2964ae964be82d3050102c0140da
Stepan <grigorian.stepan04@gmail.com>
2023-07-28 15:47:24 +0400

    Fixed character movement

--- Source/DoorProject/Private/MyCharacter.cpp
+++ Source/DoorProject/Private/MyCharacter.cpp
@@ -1,25 +1,80 @@
 #include "MyCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
 
 AMyCharacter::AMyCharacter()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+
+	CameraArm = CreateDefaultSubobject<USpringArmComponent>("CameraArm");
+	CameraArm->SetupAttachment(RootComponent); 
+	CameraArm->TargetArmLength = 300.0f;
+	CameraArm->bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(CameraArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+
 
 }
 
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (auto PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMapping, 0);
+		}
+	}
 }
 
 void AMyCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent);
 
+	Input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMyCharacter::Move);
+	Input->BindAction(RotateCameraAction, ETriggerEvent::Triggered, this, &AMyCharacter::RotateCamera);
+}
+
+void AMyCharacter::Move(const FInputActionValue& Value)
+{
+	auto input = Value.Get<FVector2D>();
+
+	FVector Forward = Controller->GetControlRotation().Vector();
+	FVector Right = Forward;
+	Swap(Right.X, Right.Y);
+	Right.X *= -1;
+
+	AddMovementInput(Forward, input.X);
+	AddMovementInput(Right, input.Y);
+}
+
+void AMyCharacter::RotateCamera(const FInputActionValue& Value)
+{
+	auto input = Value.Get<FVector>();
+
+	AddControllerYawInput(input.X);
+	AddControllerPitchInput(input.Y);
 }
--- Source/DoorProject/Public/MyCharacter.h
+++ Source/DoorProject/Public/MyCharacter.h
@@ -2,7 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "UObject/SoftObjectPtr.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 #include "MyCharacter.generated.h"
+class UInputAction;
 
 UCLASS()
 class DOORPROJECT_API AMyCharacter : public ACharacter
@@ -12,12 +18,26 @@ class DOORPROJECT_API AMyCharacter : public ACharacter
 public:
 	AMyCharacter();
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+		TObjectPtr<UInputMappingContext> InputMapping = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+		TObjectPtr<UInputAction> MoveAction = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+		TObjectPtr<UInputAction> RotateCameraAction = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera)
+		TObjectPtr<class UCameraComponent> Camera = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera)
+		TObjectPtr<class USpringArmComponent> CameraArm = nullptr;
+
 protected:
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	void Move(const FInputActionValue& Value);
+	void RotateCamera(const FInputActionValue& Value);
 };
